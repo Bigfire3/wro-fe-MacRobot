e4c84e31f1117f02a10332640ef0a067993595c6
Bigfire3 <zankerfabian5@gmail.com>
2022-10-01 09:01:18 +0200

    rename libraryFE to lib

--- Programmcode/Python/Obstacles.py
+++ Programmcode/Python/Obstacles.py
@@ -5,7 +5,7 @@ from math import sin
 from ev3dev2.motor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2._platform.ev3 import *
-import libraryFE
+import lib
 from ev3dev2.port import LegoPort
 from ev3dev2.button import Button
 from time import sleep
@@ -33,7 +33,7 @@ rotation = 0
 
 # functions
 def pass_object(sig, x_pos, height):
-    pid = libraryFE.myPID(0.1, 1, 0, 0)
+    pid = lib.myPID(0.1, 1, 0, 0)
     """
     1-3 = red, true, left
     4-6 = green, false, right
@@ -43,42 +43,42 @@ def pass_object(sig, x_pos, height):
     """
     sig_switch = 4
     if (sig < sig_switch):
-        libraryFE.SetLeds("RED")
+        lib.SetLeds("RED")
         target_x_pos = 25
         back_x_pos = 20
         sign = 1
         n = 75.97
     else:
-        libraryFE.SetLeds("GREEN")
+        lib.SetLeds("GREEN")
         target_x_pos = 230
         back_x_pos = 235
         sign = -1
         n = -26.43
 
     if (((sig < sig_switch and x_pos < target_x_pos) or (sig > sig_switch and x_pos > target_x_pos)) and height > 70): # pass
-        libraryFE.SetLeds("BLACK")
-        libraryFE.SetSteering(0)
+        lib.SetLeds("BLACK")
+        lib.SetSteering(0)
         drive_motor.on_for_rotations(50, 1.5 + abs(2 * sin(gyro_sensor.angle + rounds * 90)))
     else:
         if (((sig < sig_switch and x_pos > back_x_pos) or (sig > sig_switch and x_pos < back_x_pos)) and height > 110): # back
-            libraryFE.SetSteering(-60 * sign)
+            lib.SetSteering(-60 * sign)
             drive_motor.on_for_rotations(-50, 2)
-            libraryFE.SetSteering(0)
+            lib.SetSteering(0)
             drive_motor.on_for_rotations(50, 3)
         else: # aim
             test_value = (-0.4 * sign) * x_pos + n
             set_value = height
             pid_value = pid.run(set_value, test_value)
-            libraryFE.SetSteering(libraryFE.MaxRange(pid_value) * (2 * sign))
+            lib.SetSteering(lib.MaxRange(pid_value) * (2 * sign))
 
 # main
 if (__name__ == "__main__"):
-    libraryFE.SetLeds("BLACK")
+    lib.SetLeds("BLACK")
     steering_motor.reset()
-    libraryFE.ResetGyroSensor()
+    lib.ResetGyroSensor()
     button.wait_for_bump("enter")
     sleep(1)
-    libraryFE.SetLeds("BLACK")
+    lib.SetLeds("BLACK")
 
     while (-13 < rounds < 13):
         drive_motor.on(50)
@@ -87,13 +87,13 @@ if (__name__ == "__main__"):
                 clockwise = True
             if (clockwise):
                 rounds += 1
-                libraryFE.WaitForColor(6)
+                lib.WaitForColor(6)
         elif (color_sensor.MODE_COL_COLOR == 2): # blue
             if (rounds == 0):
                 clockwise = False
             if (not clockwise):
                 rounds -= 1
-                libraryFE.WaitForColor(6)
+                lib.WaitForColor(6)
         
         sig = pixy_cam.value(1) * 256 + pixy_cam.value(0) # Signature of largest object
         x_pos = pixy_cam.value(2)    # X-centroid of largest SIG1-object
@@ -105,10 +105,10 @@ if (__name__ == "__main__"):
             pass_object(sig, x_pos, height)
 
         else:
-            libraryFE.SetLeds("BLACK")
+            lib.SetLeds("BLACK")
             drive_motor.on(50)
-            steering_motor.on(-libraryFE.MaxRange((gyro_sensor.angle - rounds * 90) * 3))
+            steering_motor.on(-lib.MaxRange((gyro_sensor.angle - rounds * 90) * 3))
 
     drive_motor.off()
-    libraryFE.SetSteering(0)
+    lib.SetSteering(0)
     steering_motor.off()
