43123ad0d28e12167fbc953c708c2b8fa52153cc
Bigfire3 <zankerfabian5@gmail.com>
2022-10-25 16:54:52 +0200

    patch with 3 ultrasonics

--- Programmcode/Python/non_obstacles.py
+++ Programmcode/Python/non_obstacles.py
@@ -5,25 +5,52 @@ from ev3dev2.motor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2._platform.ev3 import *
 import felib
+import threading
 
-# motors
-steering_motor = MediumMotor(OUTPUT_A)
-drive_motor = MediumMotor(OUTPUT_B)
+if __name__ == "__main__":
+    # motors
+    steering_motor = MediumMotor(OUTPUT_A)
+    drive_motor = LargeMotor(OUTPUT_B)
 
-# sensors
-# ultrasonic_sensor = UltrasonicSensor(INPUT_1)
-gyro_sensor = GyroSensor(INPUT_2)
-color_sensor = ColorSensor(INPUT_3)
-ultrasonic_sensor = UltrasonicSensor(INPUT_4)
+    # sensors
+    ultrasonic_sensor1 = UltrasonicSensor(INPUT_1)
+    ultrasonic_sensor2 = UltrasonicSensor(INPUT_2)
+    ultrasonic_sensor3 = UltrasonicSensor(INPUT_3)
+    color_sensor = ColorSensor(INPUT_4)
 
-# variables
+    # variables
+    offset = 0
+
+    # main
+    steering_motor.reset()
+    line_thread = threading.Thread(target = felib.round_counter)
+    line_thread.start()
+    
+    while -13 < felib.rounds < 13:
+        drive_motor.on(50)
+        
+        if felib.rounds > 0:
+            offset = (ultrasonic_sensor3.distance_centimeters - 255) / 10
+        elif felib.rounds < 0:
+            offset = (ultrasonic_sensor3.distance_centimeters - 255) / -10
+
+        value = (ultrasonic_sensor1.distance_centimeters + offset) / (ultrasonic_sensor2.distance_centimeters - offset)
+        if value < 1:
+            value = -1 / value
+        value = value - (value / abs(value))
+        felib.set_steering(felib.max_range(value * -20))
+
+
+
+
+
+'''
 rotation = 0
 clockwise = True
 
 # main
-steering_motor.reset()
 gyro_sensor.reset()
-drive_motor.on(50)
+
 
 pid = felib.myPID(0.1, 1, 0, 1)
 
@@ -37,3 +64,4 @@ while (-13 < felib.rounds < 13):
     
 steering_motor.off()
 drive_motor.on_for_rotations(-50, 7)
+'''
